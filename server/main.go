5b592037ea6ec6b0540097082ffc6dd31b2c2415
hide0621 <fujihara_hideyuki@retail-ai.jp>
2023-04-07 17:08:47 +0900

    Unaryのserverの方を実装した

---
+++ server/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	filev1 "connect-go-practice/gen/proto"
+	"connect-go-practice/gen/proto/filev1connect"
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/bufbuild/connect-go"
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
+)
+
+type server struct{}
+
+func (s *server) ListFiles(ctx context.Context, req *connect.Request[filev1.ListFilesRequest]) (*connect.Response[filev1.ListFilesResponse], error) {
+
+	fmt.Println("ListFiles was invoked")
+
+	dir := "/Users/fujiwarahideyuki/project/connect-go-practice/strage"
+
+	paths, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	var filenames []string
+
+	for _, path := range paths {
+
+		if !path.IsDir() {
+			filenames = append(filenames, path.Name())
+		}
+	}
+
+	res := connect.NewResponse(&filev1.ListFilesResponse{
+		Filenames: filenames,
+	})
+
+	return res, nil
+
+}
+
+func main() {
+
+	a := &server{}
+
+	mux := http.NewServeMux()
+
+	path, handler := filev1connect.NewFileServiceHandler(a)
+
+	mux.Handle(path, handler)
+
+	http.ListenAndServe("localhost:8080", h2c.NewHandler(mux, &http2.Server{}))
+
+}
